fe037115689e3b4b766c0cec03ceb75d87a558e2
ziaulhoquearafat <mdarafat3167@gmail.com>
2026-06-29 22:12:28 +0600

    add nodeEnv and database_uri to config interface

--- src/config/index.ts
+++ src/config/index.ts
@@ -4,10 +4,14 @@ dotenv.config();
 
 interface Config {
   port: number;
+  nodeEnv: string;
+  database_uri: string;
 }
 
-const config = {
+const config: Config = {
   port: Number(process.env.PORT) || 3000,
+  nodeEnv: process.env.NODE_ENV || "development",
+  database_uri: process.env.DATABASE_URI || "",
 };
 
 export default config;
